b6af9d0ae3795061b301d736182c9ced88bb0886
Thien_win <tbhuynh@dessertly.com>
2012-02-24 16:41:53 -0500

    updated tester file

--- pissLoggerTester/pissLoggerTester.ino
+++ pissLoggerTester/pissLoggerTester.ino
@@ -1,22 +1,3 @@
-/*
-  SD card basic file example
- 
- This example shows how to create and destroy an SD card file 	
- The circuit:
- * SD card attached to SPI bus as follows:
- ** MOSI - pin 11
- ** MISO - pin 12
- ** CLK - pin 13
- ** CS - pin 4
- 
- created   Nov 2010
- by David A. Mellis
- updated 2 Dec 2010
- by Tom Igoe
- 
- This example code is in the public domain.
- 	 
- */
 #include <SD.h>
 
 File myFile;
@@ -25,10 +6,6 @@ void setup()
 {
   Serial.begin(9600);
   Serial.print("Initializing SD card...");
-  // On the Ethernet Shield, CS is pin 4. It's set as an output by default.
-  // Note that even if it's not used as the CS pin, the hardware SS pin 
-  // (10 on most Arduino boards, 53 on the Mega) must be left as an output 
-  // or the SD library functions will not work. 
   pinMode(10, OUTPUT);
 
   if (!SD.begin(4)) {
@@ -61,7 +38,7 @@ void setup()
 
 void loop()
 {
-  // nothing happens after setup finishes.
+
 }
 
 
